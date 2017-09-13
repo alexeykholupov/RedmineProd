e4022a2615b6d145f65fb13e2d742c9c36adb6bd
alexeykholupov <alexey.kholupov@gmail.com>
2017-09-13 16:34:37 +0300

    Adding plugin files

---
+++ init.rb
@@ -0,0 +1,7 @@
+Redmine::Plugin.register :products do
+  name 'Products plugin'
+  author 'Alexey Kholupov'
+  description 'This plugin listing products'
+  version '0.0.1'
+  author_url 'https://github.com/alexeykholupov'
+end
